3fe8631a08229461ab8066740323528ac4f3fb22
Theresa Mai <theresathimai@gmail.com>
2019-06-08 20:47:20 -0700

    Needed info for Mongo. Items are now in database.

---
+++ server.js
@@ -0,0 +1,6 @@
+// mongo log in info
+var mongoHost = process.env.MONGO_HOST || "classmongo.engr.oregonstate.edu";
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER || "cs290_maithe";
+var mongoPassword = process.env.MONGO_PASSWORD || "cs290_maithe";
+var mongoDBName = process.env.MONGO_DB_NAME || "cs290_maithe";
